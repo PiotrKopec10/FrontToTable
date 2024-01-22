f73327be813b82bedd9e0930a334deb49a7d48f7
Szufrajda <s25223@pjwstk.edu.pl>
2024-01-23 00:36:56 +0100

    Dodanie raportu do WaiterPage

---
+++ WaiterPage.js
@@ -0,0 +1,241 @@
+import React, { useState, useEffect } from 'react';
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableWithoutFeedback,
+  ImageBackground,
+  TouchableOpacity,
+  Modal,
+} from 'react-native';
+import WaiterPageStyles from './styles/WaiterPageStyles';
+import config from './config';
+
+const WaiterPage = () => {
+  const [orders, setOrders] = useState([]);
+  const [expandedSections, setExpandedSections] = useState([]);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  useEffect(() => {
+    const fetchOrders = async () => {
+      try {
+        const response = await fetch(config.endpoints.Order);
+        const result = await response.json();
+        result.sort((a, b) => {
+          if (a.orderStatus === b.orderStatus) {
+            return 0;
+          } else if (a.orderStatus === 1) {
+            return -1;
+          } else {
+            return 1;
+          }
+        });
+
+        setOrders(result);
+      } catch (error) {
+        console.error('Error fetching orders:', error);
+      }
+    };
+
+    fetchOrders();
+  }, []);
+
+  const toggleSection = (orderId) => {
+    const newExpandedSections = [...expandedSections];
+    if (newExpandedSections.includes(orderId)) {
+      const indexToRemove = newExpandedSections.indexOf(orderId);
+      newExpandedSections.splice(indexToRemove, 1);
+    } else {
+      newExpandedSections.push(orderId);
+    }
+    setExpandedSections(newExpandedSections);
+  };
+
+  const acceptOrder = async (orderId) => {
+    try {
+      console.log('Próba akceptacji zamówienia o ID:', orderId);
+
+      const newWaiterId = 1;
+      const newRestaurantId = 1;
+
+      await fetch(`${config.endpoints.Order}/${orderId}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          orderId: orderId,
+          orderTime: '2024-01-22T13:41:10.883Z',
+          orderStatus: 1,
+          orderComment: 'Naprawde Pyszne',
+          paymentMethod: 'Kartaaa',
+          waiterId: 1,
+          tableId: 1,
+          restaurantId: 1,
+        }),
+      });
+
+      const response = await fetch(config.endpoints.Order);
+      const result = await response.json();
+
+      result.sort((a, b) => {
+        if (a.orderStatus === b.orderStatus) {
+          return 0;
+        } else if (a.orderStatus === 1) {
+          return -1;
+        } else {
+          return 1;
+        }
+      });
+      setOrders(result);
+    } catch (error) {
+      console.error('Błąd przy akceptowaniu zamówienia:', error);
+    }
+  };
+
+  const MakeReadyOrder = async (orderId) => {
+    try {
+      console.log('Próba akceptacji zamówienia o ID:', orderId);
+
+      const newWaiterId = 1;
+      const newRestaurantId = 1;
+
+      await fetch(`${config.endpoints.Order}/${orderId}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          orderId: orderId,
+          orderTime: '2024-01-22T13:41:10.883Z',
+          orderStatus: 2,
+          orderComment: 'Naprawde Pyszne',
+          paymentMethod: 'Kartaaa',
+          waiterId: 1,
+          tableId: 1,
+          restaurantId: 1,
+        }),
+      });
+
+      const response = await fetch(config.endpoints.Order);
+      const result = await response.json();
+
+      result.sort((a, b) => {
+        if (a.orderStatus === b.orderStatus) {
+          return 0;
+        } else if (a.orderStatus === 2) {
+          return -1;
+        } else {
+          return 1;
+        }
+      });
+
+      setOrders(result);
+    } catch (error) {
+      console.error('Błąd przy akceptowaniu zamówienia:', error);
+    }
+  };
+
+  const renderOrderItem = ({ item }) => {
+    const orderDate = new Date(item.orderTime);
+    const formattedOrderDate = `${orderDate.getDate()}-${orderDate.getMonth() + 1}-${orderDate.getFullYear()} ${orderDate.getHours()}:${orderDate.getMinutes()}:${orderDate.getSeconds()}`;
+
+    if (item.orderStatus !== 0 && item.orderStatus !== 1) {
+      return null;
+    }
+
+    return (
+      <View style={WaiterPageStyles.orderItemContainer}>
+        <Text>{`Order ID: ${item.orderId}`}</Text>
+        <Text>{`Order Time: ${formattedOrderDate}`}</Text>
+        <Text>{`Order Comment: ${item.orderComment}`}</Text>
+        <Text>{`Payment Method: ${item.paymentMethod}`}</Text>
+        <Text>{`Table Number: ${item.tableId}`}</Text>
+        {item.orderStatus === 1 ? (
+          <View>
+            <Text style={WaiterPageStyles.newOrder}>Zamówienie zaakceptowane</Text>
+            <TouchableOpacity onPress={() => MakeReadyOrder(item.orderId)}>
+              <View style={WaiterPageStyles.readyButton}>
+                <Text style={WaiterPageStyles.readyButtonText}>Zaznacz jako gotowe</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        ) : item.orderStatus === 0 ? (
+          <View>
+            <Text style={WaiterPageStyles.newOrder}>Nowe zamówienie</Text>
+            <TouchableOpacity onPress={() => acceptOrder(item.orderId)}>
+              <View style={WaiterPageStyles.acceptButton}>
+                <Text style={WaiterPageStyles.acceptButtonText}>Akceptuj</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        ) : null}
+      </View>
+    );
+  };
+
+  const generateReport = () => {
+    // Tutaj dodaj logikę generowania raportu
+    console.log('Generowanie raportu...');
+    // Możesz użyć danych z 'orders' do obliczeń i wygenerowania raportu
+    setIsModalVisible(true);
+  };
+
+  const closeModal = () => {
+    setIsModalVisible(false);
+  };
+
+  return (
+    <View style={{ flex: 1 }}>
+      <ImageBackground source={require('./photo/BG1.png')} style={{ flex: 1, resizeMode: 'cover' }}>
+        <FlatList
+          data={orders.filter(item => item.orderStatus === 0 || item.orderStatus === 1)}
+          keyExtractor={(item) => item.orderId.toString()}
+          renderItem={({ item }) => (
+            <View key={item.orderId}>
+              <TouchableWithoutFeedback onPress={() => toggleSection(item.orderId)}>
+                <View style={WaiterPageStyles.sectionHeader}>
+                  <Text style={WaiterPageStyles.sectionHeaderText}>
+                    {expandedSections.includes(item.orderId) ? '▼' : '▶'} Order {item.orderId} - {item.orderStatus === 1 ? <Text style={WaiterPageStyles.acceptedOrder}>Zaakceptowane</Text> : <Text style={WaiterPageStyles.newOrder}>Nowe zamówienie</Text>}
+                  </Text>
+                </View>
+              </TouchableWithoutFeedback>
+              {expandedSections.includes(item.orderId) && (
+                <View style={WaiterPageStyles.sectionContent}>
+                  {renderOrderItem({ item })}
+                </View>
+              )}
+            </View>
+          )}
+        />
+        <TouchableOpacity
+          style={[WaiterPageStyles.raportButton, { margin: 15 }]}
+          onPress={generateReport}
+        >
+          <Text style={WaiterPageStyles.addButtonText}>GENERUJ RAPORT</Text>
+        </TouchableOpacity>
+
+
+<Modal visible={isModalVisible} animationType="slide" transparent={true}>
+  <View style={WaiterPageStyles.modalContainer}>
+
+    <Text style={[WaiterPageStyles.header, {color: '#FFD983', fontSize: 40}]}>RAPORT{` restaurantId`}</Text>
+    <Text style={[WaiterPageStyles.header, {color: '#FFD983', fontSize: 25}]}>{`Kelner ID: `}</Text>
+    <Text style={[WaiterPageStyles.header, {color: 'white', fontSize: 25}]}>{`Ilość obsługiwanych stolików: `}</Text>
+    <Text style={[WaiterPageStyles.header, {color: 'white', fontSize: 25}]}>{`Ilość przyjętych zamówień: `}</Text>
+    <Text style={[WaiterPageStyles.header, {color: 'white', fontSize: 25}]}>{`Najczęstsza forma płatności: `}</Text>
+    <Text style={[WaiterPageStyles.header, {color: 'white', fontSize: 25}]}>{`Kwota zarobiona przez obsłużone stoliki: `}</Text>
+
+
+    <TouchableOpacity onPress={closeModal}>
+      <Text style={WaiterPageStyles.closeModalButton}>Zamknij Raport</Text>
+    </TouchableOpacity>
+  </View>
+</Modal>
+
+      </ImageBackground>
+    </View>
+  );
+};
+
+export default WaiterPage;
---
+++ styles/WaiterPageStyles.js
@@ -0,0 +1,142 @@
+import { StyleSheet } from 'react-native';
+
+const WaiterPageStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: '#FFD983',
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  orderItemContainer: {
+    flex: 1,
+    margin: 8,
+    padding: 16,
+    borderWidth: 2,
+    borderRadius: 8,
+    backgroundColor: '#FFD983',
+    marginBottom: 16,
+  },
+  orderId: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  orderStatus: {
+    fontSize: 14,
+    color: '#888',
+  },
+  tableId: {
+    fontSize: 14,
+    color: '#888',
+    marginBottom: 8,
+  },
+  orderComment: {
+    fontSize: 14,
+    color: '#888',
+    marginBottom: 8,
+  },
+  orderDate: {
+    fontSize: 14,
+    color: '#888',
+    marginBottom: 8,
+  },
+  acceptedOrder: {
+    color: 'green',
+    fontSize: 14,
+    fontWeight: 'bold',
+    marginTop: 8,
+  },
+  newOrder: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    marginTop: 8,
+    color: 'blue',
+  },
+  newOrderContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  acceptButton: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 8,
+  },
+  acceptButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  sectionHeader: {
+    backgroundColor: '#FFD983',
+    padding: 16,
+    marginBottom: 8,
+    borderRadius: 8,
+    marginHorizontal: 20,
+    marginTop: 20,
+  },
+  sectionHeaderText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  sectionContent: {
+    padding: 16,
+    backgroundColor: 'rgba(255, 255, 255, 0.25)',
+    borderWidth: 2,
+    borderRadius: 8,
+    borderColor: '#FFD983',
+    marginTop: 8,
+  },
+  readyButton: {
+    backgroundColor: 'red',
+    padding: 10,
+    borderRadius: 8,
+    marginTop: 8,
+  },
+  readyButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  raportButton: {
+    backgroundColor: '#4CAF50',
+    padding: 10,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 15,
+  },
+  addButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  modalContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.6)',
+  },
+  closeModalButton: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 8,
+    marginTop: 20,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+    color: 'white', 
+  },
+  reportText: {
+    fontSize: 16,
+    marginBottom: 8,
+    color: 'white', 
+  },
+});
+
+export default WaiterPageStyles;
